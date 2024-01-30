1d4982023025420b02d29fed37c3d3d383ea77dd
Hristo Hristov <xstanev97@gmail.com>
2024-01-31 00:14:05 +0200

    Update ConcatenatesTwoFiles.cs

--- WorkingWithFiles/WorkingWithFiles/ConcatenatesTwoFiles.cs
+++ WorkingWithFiles/WorkingWithFiles/ConcatenatesTwoFiles.cs
@@ -1,41 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using System.IO;
-using System.Diagnostics.Metrics;
+﻿using System.IO;
 
 public class ConcatenatesTwoFiles
 {
+    public static void Main()
+    {
+        var fi = new FileInfo(@"C:\Users\UsernameT\testFile.txt");
+        var f1 = new FileInfo(@"C:\Users\UsernameT\testFile1.txt");
 
-   // public static void Main()
-   // {
+        using (var fs = fi.Open(FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read))
+        using (var sw = new StreamWriter(fs))
+        {
+            sw.WriteLine("My first text file");
+        }
 
-     //   var fi = new FileInfo(@"C:\Users\UsernameT\testFile.txt");
-     //   var f1 = new FileInfo(@"C:\Users\UsernameT\testFile1.txt");
-     //   using var fs = fi.Open(FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
-     //   using var fs1 = f1.Open(FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
-     //   using var sw = new StreamWriter(fs);
-     //   using var ww = new StreamWriter(fs1);
-     //   sw.WriteLine("My first text file");
-     //   sw.WriteLine("My second text file");
+        using (var fs1 = f1.Open(FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read))
+        using (var ww = new StreamWriter(fs1))
+        {
+            ww.WriteLine("Content from the second file.");
+        }
 
-
-        //    string[] array = Directory.GetFiles(@"C:\Users\UsernameT\", "*.txt");
-        //
-        //    System.Text.StringBuilder sb = new System.Text.StringBuilder();
-        //
-        //    for (int i = 0; i < array.Count(); i++)
-        //    {
-        //        sb.Append(System.IO.File.ReadAllText(array[i]));
-        //    }
-        //
-        //    string output = sb.ToString();
-        //    string outputFilePath = @"C:\Users\\UsernameT\MergetData.txt";
-        //
-        //    System.IO.File.WriteAllText(outputFilePath, output);
-        //}
-  //  }
+        string outputFilePath = @"C:\Users\UsernameT\MergetData.txt";
+        using (var outputWriter = new StreamWriter(outputFilePath))
+        {
+            outputWriter.Write(File.ReadAllText(fi.FullName));
+            outputWriter.Write(File.ReadAllText(f1.FullName));
+        }
+    }
 }
