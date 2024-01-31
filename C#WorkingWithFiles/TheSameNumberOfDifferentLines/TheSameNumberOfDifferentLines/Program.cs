089a9edcffdd8d9b5e5401450f34fa8be31be5f0
hristo8stanev <120580916+hristo8stanev@users.noreply.github.com>
2024-02-01 00:05:53 +0200

    Update Program.cs

--- C#WorkingWithFiles/TheSameNumberOfDifferentLines/TheSameNumberOfDifferentLines/Program.cs
+++ C#WorkingWithFiles/TheSameNumberOfDifferentLines/TheSameNumberOfDifferentLines/Program.cs
@@ -10,45 +10,39 @@ public class Program
         string inputFile = @"C:\Users\UsernameT\testFile.txt";
         string outputFile = @"C:\Users\UsernameT\testFile1.txt";
 
-     //   InsertLineNumbersToFile(inputFile, outputFile);
         CompareTwoTextFiles(inputFile, outputFile);
     }
 
-
-   // public static void InsertLineNumbersToFile(string inputFile, string outputFile)
-   // {
-   //     using (StreamReader reader = new StreamReader(inputFile))
-   //     using (StreamWriter writer = new StreamWriter(outputFile))
-   //     {
-   //         int lineNumber = 1;
-   //
-   //         while (!reader.EndOfStream)
-   //         {
-   //             string line = reader.ReadLine();
-   //             string lineWithNumber = $"{lineNumber++}. {line}";
-   //             writer.WriteLine(lineWithNumber);
-   //
-   //         }
-   //     }
-   // }
     public static void CompareTwoTextFiles(string inputFile, string outputFile)
     {
-        using (StreamReader reader = new StreamReader(inputFile))
-        using (StreamWriter writer = new StreamWriter(outputFile))
+
+        int sameLineCount = 0;
+        int differentLineCount = 0;
+
+        using (StreamReader readerA = new StreamReader(inputFile))
+        using (StreamReader readerB = new StreamReader(inputFile))
         {
-
             int lineNumber = 1;
-            
-            while (!reader.EndOfStream)
-            {
-                string line = reader.ReadLine();
-                var listA = File.ReadAllLines(inputFile);
-                var listExceptAB = File.ReadAllLines(outputFile)
-                .Except(listA);
-                string equalsLine = $"{lineNumber++}. {line}";
-                writer.WriteLine(equalsLine);
 
+            while (!readerA.EndOfStream && !readerB.EndOfStream)
+            {
+                string lineA = readerA.ReadLine();
+                string lineB = readerB.ReadLine();
+
+                if (lineA == lineB)
+                {
+                    sameLineCount++;
+                }
+                else
+                {
+                    differentLineCount++;
+                }
+
+                string resultLines = $"{lineNumber++},Line A: {lineA}, Line B: {lineB}";
+                Console.WriteLine(resultLines);
             }
         }
+        Console.WriteLine($"Number of same lines: {sameLineCount}");
+        Console.WriteLine($"Number of different lines: {differentLineCount}");
     }
 }
