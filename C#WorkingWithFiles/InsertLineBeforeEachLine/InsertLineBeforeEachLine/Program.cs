5406fd26e11e49f8fef6739cc4d612ed2411e7bf
Hristo Hristov <xstanev97@gmail.com>
2024-01-31 09:36:57 +0200

    Update Folders

---
+++ C#WorkingWithFiles/InsertLineBeforeEachLine/InsertLineBeforeEachLine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+public class InsertLineNumbers
+{
+    static void Main()
+    {
+        string inputFile = @"C:\Users\UsernameT\testFile.txt";
+        string outputFile = @"C:\Users\UsernameT\testFile1.txt";
+
+        InsertLineNumbersToFile(inputFile, outputFile);
+    }
+
+    public static void InsertLineNumbersToFile(string inputFile, string outputFile)
+    {
+        using (StreamReader reader = new StreamReader(inputFile))
+        using (StreamWriter writer = new StreamWriter(outputFile))
+        {
+            int lineNumber = 1;
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string lineWithNumber = $"{lineNumber++}. {line}";
+                writer.WriteLine(lineWithNumber);
+            }
+        }
+    }
+}
--- GenericMethodTask/GenericMethodTask/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Collections;
-
-public class DataRepo<T> where T: IEnumerable<T>
-{
-    public T Data { get; set; }
-}
