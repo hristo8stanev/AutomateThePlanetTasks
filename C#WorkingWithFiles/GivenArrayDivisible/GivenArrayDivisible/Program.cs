36beae31f5df9e287260c0efc96f65db956ce08a
hristo8stanev <120580916+hristo8stanev@users.noreply.github.com>
2024-02-01 00:54:02 +0200

    last LINQ task commit

---
+++ C#WorkingWithFiles/GivenArrayDivisible/GivenArrayDivisible/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 21, 42, 63, 14, 28, 35, 56, 77, 84, 91 };
+
+        //LINQ Method Syntax
+        var divisible = numbers.Where(num => num % 3 == 0 && num % 7 == 0);
+
+        //LINQ Query Syntax
+        // var divislbe = from number in numbers 
+                          //where number % 7 && number % 3 == 0
+                          //select number;
+
+        foreach (var num in divisible)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
