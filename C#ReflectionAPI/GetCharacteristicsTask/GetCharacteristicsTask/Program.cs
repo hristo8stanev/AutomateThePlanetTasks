57233e167eebe24bbd6f49df724c8e9162fa1e8d
hristo8stanev <120580916+hristo8stanev@users.noreply.github.com>
2024-02-02 00:20:28 +0200

    SetProperty name task

---
+++ C#ReflectionAPI/GetCharacteristicsTask/GetCharacteristicsTask/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        Worker worker = new Worker();
+        var workerType = worker.GetType();
+
+        var fullNameProperty = workerType.GetProperty("FullName", typeof(string));
+
+
+        if (fullNameProperty != null)
+        {
+            fullNameProperty.SetValue(worker, "Ivan Draganov");
+        }
+
+        var getCharacteristicsMethod = workerType.GetMethod("GetCharacteristics");
+
+       
+        if (getCharacteristicsMethod != null)
+        {
+
+            var result = getCharacteristicsMethod.Invoke(worker, new object[] { false });
+            Console.WriteLine("Method result: " + result);
+        }
+    }
+}
