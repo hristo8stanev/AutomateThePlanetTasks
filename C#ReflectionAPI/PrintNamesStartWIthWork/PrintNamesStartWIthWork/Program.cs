0bce7bc6b528f71b214f2e131e10aa6918879110
hristo8stanev <120580916+hristo8stanev@users.noreply.github.com>
2024-02-02 00:25:29 +0200

    Update Program.cs

--- C#ReflectionAPI/PrintNamesStartWIthWork/PrintNamesStartWIthWork/Program.cs
+++ C#ReflectionAPI/PrintNamesStartWIthWork/PrintNamesStartWIthWork/Program.cs
@@ -9,7 +9,7 @@ class Program
     static void Main(string[] args)
     {
         var assembly = Assembly.GetExecutingAssembly();
-        Console.WriteLine("Assembly Name: " + assembly.GetName().Name);
+        Console.WriteLine($"Assembly Name: {assembly.GetName().Name}");
 
         foreach (var type in assembly.GetTypes())
         {
