1fc4ec7ddc6d496ef1d5ca2c878c802e5f84f0c1
Hristo Hristov <xstanev97@gmail.com>
2024-02-08 23:54:25 +0200

    Update Library.js

--- JS/Library.js
+++ JS/Library.js
@@ -1,6 +1,8 @@
 const errorMessagePrice = "Price not valid";
 const errorMessageTitleLength = "Title is not valid";
 const length = 3;
+const errorMessageAuthor = "Author not valid!";
+
 class Book {
     constructor(title,author,price) {
         this.title = title
@@ -24,8 +26,11 @@ class Book {
             return this._author;
         }
         
-        set author(value){
-            this._author=value;
+        set author(value) {
+            if (/\d/.test(value.split(' ').pop())) {
+                throw new Error(errorMessageAuthor);
+            }
+            this._author = value;
         }
         
         get price(){
@@ -37,7 +42,7 @@ class Book {
             if (value < 0 || value === 0 || isNaN(value) ) {
                 throw new Error(errorMessagePrice);
             }
-            this._price = parseFloat(value.toFixed(2));
+            this._price = value
         }
         toString() {
             return `Title: ${this.title}\nAuthor: ${this.author}\nPrice: ${this.price.toFixed(1)}`;
@@ -53,5 +58,5 @@ class GoldenEditionBook extends Book{
 const book = new Book("Aster Alex", "Thief", 13.99)
 console.log(book.toString());
 
-var goldenBook = new GoldenEditionBook("Aster Alex", "IT", 13.99);
+var goldenBook = new GoldenEditionBook("Stephen King", "IT", 13.99);
 console.log(goldenBook.toString());
