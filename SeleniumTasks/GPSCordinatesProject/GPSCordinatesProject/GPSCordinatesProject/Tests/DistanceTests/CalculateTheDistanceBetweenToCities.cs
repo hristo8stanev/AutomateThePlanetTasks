5ae8dbbf1df978a7723384a5ff6125ce5b5fed1e
Hristo Hristov <xstanev97@gmail.com>
2024-02-27 13:27:15 +0200

    Update CalculateTheDistanceBetweenToCities.cs

--- SeleniumTasks/GPSCordinatesProject/GPSCordinatesProject/GPSCordinatesProject/Tests/DistanceTests/CalculateTheDistanceBetweenToCities.cs
+++ SeleniumTasks/GPSCordinatesProject/GPSCordinatesProject/GPSCordinatesProject/Tests/DistanceTests/CalculateTheDistanceBetweenToCities.cs
@@ -7,14 +7,14 @@ namespace GPSCordinatesProject.Tests.DistanceTests;
 public class CalculateTheDistanceBetweenToCities : BaseTest
 {
 
-  // //IN PROGRESS
-     [TestCase("Germany", "Unter den Linden, Friedrichstraße, 10117 Berlin", "0.01 km / 0 mi")]
-     [TestCase("Argentina", "Obelisco, Avenida Corrientes, San Nicolás, Buenos Aires", "11921.52 km / 7407.69 mi")]
-     [TestCase("Australia", "Australian Capital Territory, CGS Rowing, Alexandrina Drive, Yarralumla 2600", "16089.14 km / 9997.33 mi")]
-     [TestCase("Canada", "World Exchange Plaza, 45 O'Connor Street, Ottawa, ON K1P 5M4", "6134.52 km / 3811.82 mi")]
-     [TestCase("Japan", "unnamed road, Izumi 2, Suginami, 168-0063", "8926.31 km / 5546.55 mi")]
-     [TestCase("11455, Taipei Neihu District 開眼山步道", "Taiwan", "8965.74 km / 5571.05 mi")]
-     [TestCase("Norway", "Karl Johans gate, 0026 Oslo", "839.35 km / 521.55 mi")]
+     //IN PROGRESS
+   // [TestCase("Germany", "Unter den Linden, Friedrichstraße, 10117 Berlin", "0.01 km / 0 mi")]
+   // [TestCase("Argentina", "Obelisco, Avenida Corrientes, San Nicolás, Buenos Aires", "11921.52 km / 7407.69 mi")]
+   // [TestCase("Australia", "Australian Capital Territory, CGS Rowing, Alexandrina Drive, Yarralumla 2600", "16089.14 km / 9997.33 mi")]
+   // [TestCase("Canada", "World Exchange Plaza, 45 O'Connor Street, Ottawa, ON K1P 5M4", "6134.52 km / 3811.82 mi")]
+   // [TestCase("Japan", "unnamed road, Izumi 2, Suginami, 168-0063", "8926.31 km / 5546.55 mi")]
+   // [TestCase("11455, Taipei Neihu District 開眼山步道", "Taiwan", "8965.74 km / 5571.05 mi")]
+   // [TestCase("Norway", "Karl Johans gate, 0026 Oslo", "839.35 km / 521.55 mi")]
      [TestCase("South Africa", "unnamed road, City Centre, Cape Town, 8001", "9635.01 km / 5986.92 mi")]
      [Test]
     public void CalculateDistanceBetweenToCity(string country, string city, string distance)
